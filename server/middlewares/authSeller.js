f2ec423a815a0ad01203f0f9a6d4f5618adffd1a
mohdrehansaifi489-netizen <mohdrehansaifi489@gmail.com>
2025-11-01 23:52:05 +0530

    Update email verification in authSeller middleware

--- server/middlewares/authSeller.js
+++ server/middlewares/authSeller.js
@@ -9,7 +9,7 @@ const authSeller = async (req, resizeBy, next) => {
 
   try {
     const tokenDecode = jwt.verify(sellerToken, process.env.JWT_SECRET);
-    if (tokenDecode.email === process.env.SELLER_EMAIL) {
+    if (tokenDecode.email === process.env.EMAIL) {
       next();
     } else {
       return res.json({ success: false, message: 'Not Authorized' });
